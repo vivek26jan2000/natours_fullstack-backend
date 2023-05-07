3f8e11df182feac81fc263f6c5b7a21febf663c8
vivek sharma <vivek26jan2000@gmail.com>
2023-05-07 19:45:01 +0530

    intial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,13 +60,19 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
-  // const filteredBody = filterObj(req.body, 'name', 'email');
-  // if (req.file) filteredBody.photo = req.file.filename;
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // console.log(req.file.filename);
+  // Construct the URL to the uploaded file
+  const fileUrl = `${req.protocol}://${req.get('host')}/public/img/users/${
+    req.file.filename
+  }`;
+
+  console.log(filteredBody);
 
   // 3) Update user document
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, req.body, {
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
   });
@@ -74,7 +80,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      user: updatedUser
+      user: updatedUser,
+      fileUrl
     }
   });
 });
